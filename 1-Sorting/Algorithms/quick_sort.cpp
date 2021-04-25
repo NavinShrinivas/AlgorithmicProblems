27e4a5e13c5fa02d07140a57e71f572047c783c1
Navin Shrinivas <karupal2002@gmail.com>
2021-04-25 19:29:16 +0530

    finished all sorting algorithms
    Signed-off-by: Navin Shrinivas <karupal2002@gmail.com>
    
    should solve more problems on sorting in the coming weeks
    the calibure of problems being solvecd is planned to be
    quite high.

--- 1-Sorting/Algorithms/quick_sort.cpp
+++ 1-Sorting/Algorithms/quick_sort.cpp
@@ -10,7 +10,60 @@ void swap(int a[],int i,int j)
     a[i]=a[j];
     a[j]=temp;
 }
+
+int pivotplacer(int a[],int l,int r)
+{
+    int i=l;
+    int pivot=a[r];
+    //higly experimental code
+    // for(int j=l;j<r;j++)
+    // {
+    //     if(a[j]>pivot && i==l-1)
+    //     {
+    //         i=j;
+    //     }
+    //     else if(a[j]<pivot && i!=l-1)
+    //     {
+    //         swap(a,i,j);
+    //         i=j;
+    //     }
+    // }
+    for (int j=l;j<r;j++)
+    {
+        if(a[j]<pivot)
+        {
+               swap(a,i,j);
+               i++;
+        }
+    }
+    swap(a,i,r);
+    return i;
+}
+
+void quicksort(int a[],int l,int r)
+{
+    if(l>=r)
+    {
+        return;
+    }
+    int pi=pivotplacer(a,l,r);
+    quicksort(a,l,pi-1);
+    quicksort(a,pi+1,r);
+}
+
 int main()
 {
-    
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0 ; i<n ; i++){
+        int c;
+        cin>>c;
+        a[i]=c;
+    }
+    quicksort(a,0,n);
+    for(int i=0 ; i<n ; i++){
+        cout<<a[i]<<" ";
+    }
 }
+
