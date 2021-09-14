df768df68808782bb08e13962aa34059acca69a6
NavinShrinivas <karupal2002@gmail.com>
2021-09-14 14:14:47 +0530

    few 'more UE course programs

---
+++ UE20CS202/QueueArrays/1.h
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+#include<stdbool.h>
+
+#define QUEUESIZE 100
+
+void queuepush(int* queue , int* top , int* front,int e);
+void queuepop(int* queue,int* top , int* front);
+void queuepeek(int* queue , int* top , int* front);
+void queuedisplay(int* queue , int* top, int* front);
+
