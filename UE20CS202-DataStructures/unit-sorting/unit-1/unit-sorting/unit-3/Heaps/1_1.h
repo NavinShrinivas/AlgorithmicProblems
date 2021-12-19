23de947ec9735b9f3dc8747860da84451cab5948
NavinShrinivas <karupal2002@gmail.com>
2021-12-19 10:35:29 +0530

    wut,where did that come from

--- UE20CS202-DataStructures/unit-sorting/unit-1/unit-sorting/unit-3/Heaps/1_1.h
+++
@@ -1,6 +0,0 @@
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <stdbool.h> 
-#define ARRSIZE 1000 
- 
-void HeapInsert(int* tree , int d);
--- UE20CS202-DataStructures/unit-sorting/unit-1/unit-sorting/unit-5/TrieTrees/formatter.py
+++
@@ -1,8 +0,0 @@
-file = open("input.txt","r")
-
-con = file.readlines();
-file2=open("out.txt","a+")
-
-for i in con:
-    file2.write(i.strip("0123456789\t "))
-    print(i.strip("0123456789\t "))
