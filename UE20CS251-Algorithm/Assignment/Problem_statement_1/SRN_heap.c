84e931a93ff6814306672af4bba2ed1e6ba569fa
P K Navin Shirnivas <karupal2002@gmail.com>
2022-02-16 21:47:03 +0530

    Have made some progress in assignment
    Signed-off-by: P K Navin Shirnivas <karupal2002@gmail.com>
    
    I have written random codes, I am not very sure they are perfectly right

--- UE20CS251-Algorithm/Assignment/Problem_statement_1/SRN_heap.c
+++ UE20CS251-Algorithm/Assignment/Problem_statement_1/SRN_heap.c
@@ -23,6 +23,10 @@ void init_heap(heap_t *heap, int max_size) {
   heap->arr = (int *)calloc(max_size, sizeof(int));
   // sizeof int as its sometimes compiler and system dependant
   // default value 0 cus of calloc
+  // as observed in test in client file, 0 is a posible value, hence -1 is
+  // indicating empty :
+  for (int i = 0; i < heap->max_size; i++)
+    heap->arr[i] = -1;
 }
 
 // Insert element "key" to heap "heap"
@@ -31,7 +35,7 @@ void init_heap(heap_t *heap, int max_size) {
 void insert(heap_t *heap, int key, int *count_ptr) {
   // the last free space is pointed to by size varaible
   // hence insert there first
-  heap->size += 1;
+  heap->size += 1; // Doesn't allow values to occupy 0th index
   heap->arr[heap->size] = key;
   int i = heap->size;
   int is_maxheap = 0;
@@ -54,22 +58,95 @@ void insert(heap_t *heap, int key, int *count_ptr) {
 /*// *Removes and Returns* the maximum element in the heap*/
 /*// and store the number of key comparisons made in the*/
 /*// location pointed to by count_ptr.*/
-/*int extract_max(heap_t *heap, int *count_ptr) {}*/
+int extract_max(heap_t *heap, int *count_ptr) {
+  // the deleting node (root) msut be replaced with rightmost element
+  int right_farthest = 0;
+  for (int i = 0; i < heap->max_size; i++) {
+    if (heap->arr[i] != -1) // not key comparisons
+      right_farthest = i;
+  }
+  int largest_value = heap->arr[1];
+  heap->size -= 1;
+  heap->arr[1] = heap->arr[right_farthest]; // replacing
+  heap->arr[right_farthest] = -1;           // deleting
+
+  int is_maxheap = 0;
+  int i = 1;
+  int largest_pos = i;
+  while (!is_maxheap) {
+    largest_pos = i;
+    int r = (largest_pos * 2) + 1;
+    int l = largest_pos * 2;
+
+    if (l < heap->max_size &&
+        heap->arr[l] > heap->arr[largest_pos]) { // key comparison
+      largest_pos = l;
+      *(count_ptr) += 1;
+    }
+    if (r < heap->max_size &&
+        heap->arr[r] > heap->arr[largest_pos]) { // key comparison
+      largest_pos = r;
+      *(count_ptr) += 1;
+    }
+    if (largest_pos != i) { // not key comparison
+      int temp_ = heap->arr[i];
+      heap->arr[i] = heap->arr[largest_pos];
+      heap->arr[largest_pos] = temp_;
+      i = largest_pos;
+    } else {
+      is_maxheap = 1;
+    }
+  }
+  return largest_value;
+}
 
 /*// Searches for the element key in the heap*/
 /*// Returns the element if found, else -1*/
-/*int search(const heap_t *heap, int key, int *count_ptr) {}*/
+int search(const heap_t *heap, int key, int *count_ptr) {
+  // instead of searchign through tree, making use of the array structure
+  // We are doing linear search
+  int i = 1;
+  while (i < heap->max_size && heap->arr[i] != key) {
+    *(count_ptr) += 1;
+    i++;
+  }
+  if (i == heap->max_size)
+    return -1;
+  else
+    return heap->arr[i];
+}
 
 /*// Returns the maximum value of the element in the heap*/
 /*// and store the number of key comparisons made in the*/
 /*// location pointed to by count_ptr.*/
-/*int find_max(const heap_t *heap, int *count_ptr) {}*/
+int find_max(const heap_t *heap, int *count_ptr) {
+  // max heap, thus no need of any comparisons we already know the posistion of
+  // max element
+  *(count_ptr) = 0;
+  return heap->arr[1];
+}
 
 /*// Returns the minimum value in the heap*/
-/*int find_min(const heap_t *heap, int *count_ptr) {}*/
+int find_min(const heap_t *heap, int *count_ptr) {
+  // Again, no need to follow tree paths, just linear search in the array :
+  int smallest = heap->arr[1];
+  int i = 2;
+  while (i < heap->max_size && heap->arr[i] != -1) {
+    // the key comparison takes place every time this loop iterates
+    *(count_ptr) += 1;
+    if (smallest > heap->arr[i]) { // key comparison
+      smallest = heap->arr[i];
+    }
+    i++;
+  }
+  return smallest;
+}
 
-/*// Clears the heap for reuse*/
-/*void clear_heap(heap_t *heap) {}*/
+void clear_heap(heap_t *heap) {
+  heap->size = 0;
+  for (int i = 0; i < heap->max_size; i++)
+    heap->arr[i] = -1;
+}
 
 /*// Frees all resources acquired to initialize heap*/
-/*void free_heap(heap_t *heap) {}*/
+void free_heap(heap_t *heap) { free(heap); }
--- UE20CS251-Algorithm/Assignment/Problem_statement_1/heap_client.c
+++ UE20CS251-Algorithm/Assignment/Problem_statement_1/heap_client.c
@@ -60,156 +60,149 @@ int main() {
 
   int a[] = {6, 3, 7, 4, 8, 1, 5, 9, 2, 0};
   copy_array(&h, a, 10);
-  for (int i = 1; i <= 10; i++) {
-    printf("%d ", h.arr[i]);
-  }
-  /*    int c = 0;*/
-  /*assert(find_max(&h, &c) == 9);*/
-  /*c = 0;*/
-  /*assert(extract_max(&h, &c) == 9);*/
-  /*c = 0;*/
-  /*assert(search(&h, 4, &c) == 4);*/
-  /*c = 0;*/
-  /*assert(search(&h, 10, &c) == -1);*/
-  /*c = 0;*/
-  /*assert(find_max(&h, &c) == 8);*/
-  /*c = 0;*/
-  /*assert(find_min(&h, &c) == 0);*/
-  /*c = 0;*/
-  /*clear_heap(&h);*/
+  /*
+   *  // my shit
+   *  for (int i = 1; i <= 10; i++) {
+   *    printf("%d ", h.arr[i]);
+   *  }
+   *  int ci;
+   *  int max = extract_max(&h, &ci);
+   *  printf("\n %d \n", max);
+   *  for (int i = 1; i <= 10; i++) {
+   *    printf("%d ", h.arr[i]);
+   *  }
+   *
+   *  //======
+   */
+
+  int c = 0;
+  assert(find_max(&h, &c) == 9);
+  c = 0;
+  assert(extract_max(&h, &c) == 9);
+  c = 0;
+  assert(search(&h, 4, &c) == 4);
+  c = 0;
+  assert(search(&h, 10, &c) == -1);
+  c = 0;
+  assert(find_max(&h, &c) == 8);
+  c = 0;
+  assert(find_min(&h, &c) == 0);
+  c = 0;
+  clear_heap(&h);
 
   /*
-  **
-  ** Analysis of asymptotic complexities of Heap operations
-  **
-  */
+   *
+   *  Analysis of asymptotic complexities of Heap operations
+   *
+   */
+  printf("DS: Max Heap\n"
+         "Operation: Insert\n"
+         "Input: In order insert\n\n");
+  ordered_insert(&h, n, 1);
+  clear_heap(&h);
 
-  /*printf("DS: Max Heap\n"*/
-  /*"Operation: Insert\n"*/
-  /*"Input: In order insert\n\n");*/
-  /*ordered_insert(&h, n, 1);*/
-  /*clear_heap(&h);*/
+  printf("\nInput: Random Inserts\n\n");
+  randomize_insert(&h, n, 1);
+  clear_heap(&h);
 
-  /*printf("\nInput: Random Inserts\n\n");*/
-  /*randomize_insert(&h, n, 1);*/
-  /*clear_heap(&h);*/
+  printf("\nInput: Reversed Inserts\n\n");
+  reversed_insert(&h, n, 1);
+  clear_heap(&h);
 
-  /*printf("\nInput: Reversed Inserts\n\n");*/
-  /*reversed_insert(&h, n, 1);*/
-  /*clear_heap(&h);*/
+  printf("\nOperation: Find Max\n"
+         "\nInput: Random inserted Heap\n\n");
+  for (int i = 1; i <= n; ++i) {
+    if (print_if(i)) {
+      randomize_insert(&h, n, 0);
+      int c = 0;
+      find_max(&h, &c);
+      printf("%d,%d\n", i, c);
+      clear_heap(&h);
+    }
+  }
+  printf("\nInput: Ordered inserted Heap\n\n");
+  for (int i = 1; i <= n; ++i) {
+    if (print_if(i)) {
+      ordered_insert(&h, n, 0);
+      int c = 0;
+      find_max(&h, &c);
+      printf("%d,%d\n", i, c);
+      clear_heap(&h);
+    }
+  }
+  printf("\nOperation: Delete_max\n"
+         "\nInput: Random inserted tree\n\n");
+  for (int i = 1; i <= n; ++i) {
+    if (print_if(i)) {
+      int c = 0;
+      randomize_insert(&h, i, 0);
+      extract_max(&h, &c);
+      printf("%d,%d\n", i, c);
+      clear_heap(&h);
+    }
+  }
+  printf("\nInput: Ordered delete on random inserted heap\n\n");
+  randomize_insert(&h, n, 0);
+  for (int i = n; i >= 1; --i) {
+    int c = 0;
+    extract_max(&h, &c);
+    if (print_if(i)) {
+      printf("%d,%d\n", i, c);
+    }
+  }
+  clear_heap(&h);
+  printf("\nOperation: Search\n"
+         "\nInput: Random element in random tree\n\n");
+  for (int i = 1; i < n; ++i) {
+    if (print_if(i)) {
+      int c = 0;
+      randomize_insert(&h, i, 0);
+      search(&h, rand() % i + 1, &c);
+      printf("%d,%d\n", i, c);
+      clear_heap(&h);
+    }
+  }
 
-  /*printf("\nOperation: Find Max\n"*/
-  /*"\nInput: Random inserted Heap\n\n");*/
-  /*for (int i = 1; i <= n; ++i)*/
-  /*{*/
-  /*if (print_if(i))*/
-  /*{*/
-  /*randomize_insert(&h, n, 0);*/
-  /*int c = 0;*/
-  /*find_max(&h, &c);*/
-  /*printf("%d,%d\n", i, c);*/
-  /*clear_heap(&h);*/
-  /*}*/
-  /*}*/
-  /*printf("\nInput: Ordered inserted Heap\n\n");*/
-  /*for (int i = 1; i <= n; ++i)*/
-  /*{*/
-  /*if (print_if(i))*/
-  /*{*/
-  /*ordered_insert(&h, n, 0);*/
-  /*int c = 0;*/
-  /*find_max(&h, &c);*/
-  /*printf("%d,%d\n", i, c);*/
-  /*clear_heap(&h);*/
-  /*}*/
-  /*}*/
-  /*printf("\nOperation: Delete_max\n"*/
-  /*"\nInput: Random inserted tree\n\n");*/
-  /*for (int i = 1; i <= n; ++i)*/
-  /*{*/
-  /*if (print_if(i))*/
-  /*{*/
-  /*int c = 0;*/
-  /*randomize_insert(&h, i, 0);*/
-  /*extract_max(&h, &c);*/
-  /*printf("%d,%d\n", i, c);*/
-  /*clear_heap(&h);*/
-  /*}*/
-  /*}*/
-  /*printf("\nInput: Ordered delete on random inserted heap\n\n");*/
-  /*randomize_insert(&h, n, 0);*/
-  /*for (int i = n; i >= 1; --i)*/
-  /*{*/
-  /*int c = 0;*/
-  /*extract_max(&h, &c);*/
-  /*if (print_if(i))*/
-  /*{*/
-  /*printf("%d,%d\n", i, c);*/
-  /*}*/
-  /*}*/
-  /*clear_heap(&h);*/
-  /*printf("\nOperation: Search\n"*/
-  /*"\nInput: Random element in random tree\n\n");*/
-  /*for (int i = 1; i < n; ++i)*/
-  /*{*/
-  /*if (print_if(i))*/
-  /*{*/
-  /*int c = 0;*/
-  /*randomize_insert(&h, i, 0);*/
-  /*search(&h, rand() % i + 1, &c);*/
-  /*printf("%d,%d\n", i, c);*/
-  /*clear_heap(&h);*/
-  /*}*/
-  /*}*/
+  printf("\nInput: Random element in ordered tree\n\n");
+  for (int i = 1; i < n; ++i) {
+    if (print_if(i)) {
+      int c = 0;
+      ordered_insert(&h, i, 0);
+      search(&h, rand() % i + 1, &c);
+      printf("%d,%d\n", i, c);
+      clear_heap(&h);
+    }
+  }
 
-  /*printf("\nInput: Random element in ordered tree\n\n");*/
-  /*for (int i = 1; i < n; ++i)*/
-  /*{*/
-  /*if (print_if(i))*/
-  /*{*/
-  /*int c = 0;*/
-  /*ordered_insert(&h, i, 0);*/
-  /*search(&h, rand() % i + 1, &c);*/
-  /*printf("%d,%d\n", i, c);*/
-  /*clear_heap(&h);*/
-  /*}*/
-  /*}*/
-
-  /*printf("\nInput: Element not present in ordered tree\n\n");*/
-  /*for (int i = 1; i < n; ++i)*/
-  /*{*/
-  /*if (print_if(i))*/
-  /*{*/
-  /*int c = 0;*/
-  /*ordered_insert(&h, i, 0);*/
-  /*search(&h, rand() + 1, &c);*/
-  /*printf("%d,%d\n", i, c);*/
-  /*clear_heap(&h);*/
-  /*}*/
-  /*}*/
-  /*printf("\nOperation: Find min\n"*/
-  /*"\nInput: Ordered input\n\n");*/
-  /*for (int i = 1; i <= n; ++i)*/
-  /*{*/
-  /*if (print_if(i))*/
-  /*{*/
-  /*int c = 0;*/
-  /*ordered_insert(&h, i, 0);*/
-  /*find_min(&h, &c);*/
-  /*printf("%d,%d\n", i, c);*/
-  /*clear_heap(&h);*/
-  /*}*/
-  /*}*/
-  /*printf("\nInput: Random input\n\n");*/
-  /*for (int i = 1; i <= n; ++i)*/
-  /*{*/
-  /*if (print_if(i))*/
-  /*{*/
-  /*int c = 0;*/
-  /*randomize_insert(&h, i, 0);*/
-  /*find_min(&h, &c);*/
-  /*printf("%d,%d\n", i, c);*/
-  /*clear_heap(&h);*/
-  /*}*/
-  /*}*/
+  printf("\nInput: Element not present in ordered tree\n\n");
+  for (int i = 1; i < n; ++i) {
+    if (print_if(i)) {
+      int c = 0;
+      ordered_insert(&h, i, 0);
+      search(&h, rand() + 1, &c);
+      printf("%d,%d\n", i, c);
+      clear_heap(&h);
+    }
+  }
+  printf("\nOperation: Find min\n"
+         "\nInput: Ordered input\n\n");
+  for (int i = 1; i <= n; ++i) {
+    if (print_if(i)) {
+      int c = 0;
+      ordered_insert(&h, i, 0);
+      find_min(&h, &c);
+      printf("%d,%d\n", i, c);
+      clear_heap(&h);
+    }
+  }
+  printf("\nInput: Random input\n\n");
+  for (int i = 1; i <= n; ++i) {
+    if (print_if(i)) {
+      int c = 0;
+      randomize_insert(&h, i, 0);
+      find_min(&h, &c);
+      printf("%d,%d\n", i, c);
+      clear_heap(&h);
+    }
+  }
 }
